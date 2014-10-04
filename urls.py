f526ce0881fa913614779b9b7c4028d91d7455ed
Jenny Shen <jenshen@mit.edu>
2014-10-04 09:38:07 -0400

    URLs and reset email change

--- urls.py
+++ urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import *
 from django.conf import settings
+from django.views.generic import TemplateView
+from django.contrib.auth import views as auth_views
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -14,11 +16,15 @@ urlpatterns = patterns('',
     url(r'^accounts/login/$', 'django.contrib.auth.views.login', {'template_name': 'accounts/login.html'}),
     url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
     url(r'^accounts/change/$', 'django.contrib.auth.views.password_change', {'template_name': 'accounts/password_change.html', 'post_change_redirect': '/accounts/change/done/'}),
-    url(r'^accounts/change/done/$', 'django.contrib.auth.views.password_change_done', {'template_name': 'accounts/password_change_done.html'}),
-    url(r'^accounts/reset/$', 'django.contrib.auth.views.password_reset', {'template_name': 'accounts/password_reset.html', 'email_template_name': 'accounts/password_reset_email.html'}),
-    url(r'^accounts/reset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', 'django.contrib.auth.views.password_reset_confirm', {'template_name': 'accounts/password_reset_confirm.html', 'post_reset_redirect' : '/accounts/login/'}),
-    url(r'^accounts/reset/done/$', 'django.contrib.auth.views.password_reset_done', {'template_name': 'accounts/password_reset_done.html'}),
-
+    url(r'^accounts/change/done/$', 'django.contrib.auth.views.password_change_done', {'template_name': 'accounts/password_change_done.html'}, name='password_change_done'),
+    url(r'^accounts/reset/$', 'django.contrib.auth.views.password_reset', {'template_name': 'accounts/password_reset.html', 'email_template_name': 'accounts/password_reset_email.html'}, name='password_reset'),
+    url(r'^accounts/reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>.+)/$', 'django.contrib.auth.views.password_reset_confirm', {'template_name': 'accounts/password_reset_confirm.html', 'post_reset_redirect' : '/accounts/login/'}, name='password_reset_confirm'),
+    # make password reset links back-compatible with 1.5- generated links
+    url(r'^accounts/reset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', 'django.contrib.auth.views.password_reset_confirm_uidb36'),
+    url(r'^accounts/reset/done/$', 'django.contrib.auth.views.password_reset_done', {'template_name': 'accounts/password_reset_done.html'}, name='password_reset_done'),
+    url(r'^accounts/reset/complete/$',
+                    'django.contrib.auth.views.password_reset_complete',
+                    name='password_reset_complete'),
     #url(r'^sistersonly/events/$', 'website.views.sistersonly_events'),
     #url(r'^sistersonly/events/(\d+)/$', 'website.views.sistersonly_events_attendance'),
     url(r'^sistersonly/attendance/$', 'website.views.sistersonly_attendance'),
@@ -31,7 +37,7 @@ urlpatterns = patterns('',
     url(r'^sistersonly/elections/loi/results/$', 'website.views.sistersonly_elections_loi_results'),
     url(r'^sistersonly/elections/slating/$', 'website.views.sistersonly_elections_slating'),
     url(r'^sistersonly/elections/slating/results/$', 'website.views.sistersonly_elections_slating_results'),
-
+    
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
@@ -40,4 +46,8 @@ urlpatterns = patterns('',
 
     # For static media files served in a development environment
     (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
+
+    # robots.txt
+    url(r'^robots\.txt/$', TemplateView.as_view(template_name='robots.txt',
+                                            content_type='text/plain')),
 )
